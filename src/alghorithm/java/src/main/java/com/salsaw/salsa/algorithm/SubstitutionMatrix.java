bdf3632229a3e33cb9eecb70fb34ddc3abf368fa
Fabio Cesarato <fabio.cesarato@gmail.com>
2015-02-06 19:34:06 +0100

    [ENH] Delete unused import

--- src/alghorithm/java/src/main/java/com/salsaw/salsa/algorithm/SubstitutionMatrix.java
+++ src/alghorithm/java/src/main/java/com/salsaw/salsa/algorithm/SubstitutionMatrix.java
@@ -15,9 +15,7 @@
  */
 package com.salsaw.salsa.algorithm;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 

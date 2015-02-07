965c1d55468c3c96e75604ca21f23aa5c03b7276
Fabio Cesarato <fabio.cesarato@gmail.com>
2015-02-07 19:38:30 +0100

    [ADD] Add enum TerminalGAPsStrategy

---
+++ src/alghorithm/java/src/main/java/com/salsaw/salsa/algorithm/TerminalGAPsStrategy.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2015 Alessandro Daniele, Fabio Cesarato, Andrea Giraldin
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.salsaw.salsa.algorithm;
+
+/**
+ * A terminal GAP is a GAP in the beginning or in the end of a sequence. Usually, the penalties for a
+ * terminal GAP is different from the others.
+ * 
+ * @author Alessandro Daniele, Fabio Cesarato, Andrea Giraldin
+ *
+ */
+public enum TerminalGAPsStrategy {
+	/**
+	 * as other GAPs, a terminal GAP has both GOP and GEP
+	 */
+	BOTH_PENALTIES,
+	/**
+	 * there is no penalty for opening a terminal GAP, only for extending it
+	 */
+	ONLY_GEP
+}

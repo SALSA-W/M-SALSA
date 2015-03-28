38910bc52053283d163a65f3a1ab91c55b005e10
Fabio Cesarato <fabio.cesarato@gmail.com>
2015-03-28 18:51:31 +0100

    [ENH] Add immidiate bind for SALSA parameters on UI

--- src/webui/src/main/java/com/salsaw/msalsa/SalsaParametersForm.java
+++ src/webui/src/main/java/com/salsaw/msalsa/SalsaParametersForm.java
@@ -3,6 +3,7 @@ package com.salsaw.msalsa;
 import com.salsaw.msalsa.algorithm.TerminalGAPsStrategy;
 import com.salsaw.msalsa.cli.SalsaParameters;
 import com.salsaw.msalsa.cli.ScoringMatrix;
+import com.salsaw.msalsa.clustal.ClustalType;
 import com.vaadin.data.fieldgroup.FieldGroup;
 import com.vaadin.data.fieldgroup.FieldGroup.CommitException;
 import com.vaadin.data.fieldgroup.PropertyId;
@@ -19,6 +20,9 @@ public class SalsaParametersForm extends CustomComponent {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	@PropertyId("clustalType")
+    ComboBox clustalTypeField = new ComboBox("Clustal");
 
 	@PropertyId("GOP")
     TextField GOPField = new TextField("GOP");
@@ -43,27 +47,48 @@ public class SalsaParametersForm extends CustomComponent {
     
 	public SalsaParametersForm(BeanItem<SalsaParameters> salsaParametersBeanItem) {		
         FormLayout formLayout = new FormLayout();
+        
+        clustalTypeField.setNullSelectionAllowed(false);
+        clustalTypeField.setImmediate(true);
+        for(ClustalType clustalType : ClustalType.values()) {
+        	clustalTypeField.addItem(clustalType);
+        }
+        formLayout.addComponent(clustalTypeField);
+        
         scoringMatrixField.setNullSelectionAllowed(false);
+        scoringMatrixField.setImmediate(true);
         for(ScoringMatrix scoringMatrix : ScoringMatrix.values()) {
         	scoringMatrixField.addItem(scoringMatrix);
         }
-        scoringMatrixField.setImmediate(true);
         formLayout.addComponent(scoringMatrixField);
-        minIterationsField.setImmediate(true);       
-        formLayout.addComponent(minIterationsField);        
+        
+        minIterationsField.setImmediate(true);        
+        formLayout.addComponent(minIterationsField);
+        
+        GOPField.setImmediate(true);
         formLayout.addComponent(GOPField);
+        
+        GEPField.setImmediate(true);
         formLayout.addComponent(GEPField);
+        
+        probabilityOfSplitField.setImmediate(true);
         formLayout.addComponent(probabilityOfSplitField);
-        formLayout.addComponent(gammaField);
-        formLayout.addComponent(terminalGAPsStrategyField);
+        
+        gammaField.setImmediate(true);
+        formLayout.addComponent(gammaField);        
+       
+        
         terminalGAPsStrategyField.setNullSelectionAllowed(false);
+        terminalGAPsStrategyField.setImmediate(true);
         for(TerminalGAPsStrategy terminalGAPsStrategy : TerminalGAPsStrategy.values()) {
         	terminalGAPsStrategyField.addItem(terminalGAPsStrategy);
         }
+        formLayout.addComponent(terminalGAPsStrategyField);
         
         // Data binding based on attributes
         FieldGroup binder = new FieldGroup(salsaParametersBeanItem);
         binder.bindMemberFields(this);
+        binder.setBuffered(false);
 		
         formLayout.setSizeUndefined();
         

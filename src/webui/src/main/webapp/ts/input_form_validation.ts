a41cd5c195d8bf8af7d281d586de98ae218f4e9c
Fabio Cesarato <fabio.cesarato@gmail.com>
2015-11-29 20:06:41 +0100

    [ADD] Add TypeScript file for validate input

---
+++ src/webui/src/main/webapp/ts/input_form_validation.ts
@@ -0,0 +1,25 @@
+/**
+ * New typescript file
+ */
+let aminoAcids: Array<string> = [
+    "A",//Ala
+    "C",//Cys
+    "D",//Asp
+    "E",//Glu
+    "F",//Phe
+    "G",//Gly
+    "H",//His
+    "I",//Ile
+    "K",//Lys
+    "L",//Leu
+    "M",//Met
+    "N",//Asn
+    "P",//Pro
+    "Q",//Gln
+    "R",//Arg
+    "S",//Ser
+    "T",//Thr
+    "V",//Val
+    "W",//Trp
+    "Y",//Tyr
+];

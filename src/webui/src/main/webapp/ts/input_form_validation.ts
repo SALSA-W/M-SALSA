8c003280bdc0df681a4272f48c09ef0ea683d2f4
Fabio Cesarato <fabio.cesarato@gmail.com>
2015-11-30 00:19:07 +0100

    [ENH] First draft of input validation
    Add work in progress TypeScript code to validate input sequence

--- src/webui/src/main/webapp/ts/input_form_validation.ts
+++ src/webui/src/main/webapp/ts/input_form_validation.ts
@@ -1,7 +1,7 @@
 /**
  * New typescript file
  */
-let aminoAcids: Array<string> = [
+const aminoAcids: Array<string> = [
     "A",//Ala
     "C",//Cys
     "D",//Asp
@@ -23,3 +23,70 @@ let aminoAcids: Array<string> = [
     "W",//Trp
     "Y",//Tyr
 ];
+
+const NewLine: string = "\n";
+const EmptyString: string = "";
+const WhiteSpace: string = " ";
+const HTMLNewLine: string = "<br>";
+
+class ProteinValidator {
+    private sequence: string;
+    private description: string;
+    private htmlSequence: string = EmptyString;
+    private errors: Array<string> = [];
+
+    constructor(input: string) {
+        if (input[0] != ">") {
+            this.errors.push("Missing > at descprion beginning. Unable to manage first line.");
+            this.sequence = input;
+        }
+        else {
+            // Get first line as description
+            let firstNewLineIndex = input.indexOf(NewLine);
+            this.description = input.substring(0, firstNewLineIndex);
+            // Use the rest of string as sequence
+            this.sequence = input.substring(firstNewLineIndex + 1, input.length);
+        }
+
+        for (var i = 0, len = this.sequence.length; i < len; i++) {
+            if (this.sequence[i] === NewLine) {
+                this.htmlSequence += HTMLNewLine;
+            }
+            else {
+                // Skip white space from validation
+                if (this.sequence[i] != WhiteSpace) {
+                    if (aminoAcids.indexOf(this.sequence[i]) === -1) {
+                        // Not found
+                        this.htmlSequence += this.AddHTMLErrorDecorator(this.sequence[i]);
+                        this.errors.push("Invalid character " + this.sequence[i]);
+                    }
+                    else {
+                        this.htmlSequence += this.sequence[i];
+                    }
+                }
+            }
+        }
+    }
+
+    private AddHTMLErrorDecorator(invalidCharacter: string): string {
+        // Use bootstrap style
+        return '<strong class="text-danger">' + invalidCharacter + '</strong>';
+    }
+
+    public static ComposeErrorDiv(proteinValidator: ProteinValidator): string {
+        if (proteinValidator.errors.length == 0) {
+            return null;
+        }
+
+        let result = '<div class="alert"><strong class="text-danger">Validation Errors!</strong>' + HTMLNewLine;
+        result += '<p>In sequence <strong>' + proteinValidator.description + '</strong></p>';
+        result += proteinValidator.htmlSequence;
+        result += '<div class="alert alert-danger">The errors are:'
+        proteinValidator.errors.forEach((error) => result += HTMLNewLine + error);
+        // Alert Error div
+        result += "</div>";
+        // Alert div
+        result += "</div>";
+        return result;
+    }
+}

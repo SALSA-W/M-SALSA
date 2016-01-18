d98d9a4450a5f18c0600d790e5d0b668cc41076d
Fabio Cesarato <fabio.cesarato@gmail.com>
2016-01-18 22:35:25 +0100

    [ENH] Add doc to DistanceMatrix and set final on methods

--- src/alghorithm/java/src/main/java/com/salsaw/msalsa/algorithm/DistanceMatrix.java
+++ src/alghorithm/java/src/main/java/com/salsaw/msalsa/algorithm/DistanceMatrix.java
@@ -25,62 +25,71 @@ import com.salsaw.msalsa.algorithm.exceptions.SALSAException;
  * @author Alessandro Daniele, Fabio Cesarato, Andrea Giraldin
  *
  */
-public class DistanceMatrix {
-	
-	int numberOfSequences;
-	float[] distMatrix;
-	String[] names;
+public final class DistanceMatrix {
 
-	public DistanceMatrix(final InputStream distanceMatrixStream) {				
-		try(Scanner scanner = new Scanner(distanceMatrixStream))
-		{
+	private final int numberOfSequences;
+	private final float[] distMatrix;
+	private final String[] names;
+
+	public DistanceMatrix(final InputStream distanceMatrixStream) {
+		try (Scanner scanner = new Scanner(distanceMatrixStream)) {
 			// First line contains the number of sequences
 			this.numberOfSequences = scanner.nextInt();
-			this.distMatrix = new float[numberOfSequences*numberOfSequences];
+			this.distMatrix = new float[numberOfSequences * numberOfSequences];
 			this.names = new String[numberOfSequences];
-								
-			// Read the values for the matrix value (the matrix is alphabet x alphabet)
-			for (int i=0; i<this.numberOfSequences;i++){
+
+			// Read the values for the matrix value (the matrix is alphabet x
+			// alphabet)
+			for (int i = 0; i < this.numberOfSequences; i++) {
 				// Read name
 				names[i] = scanner.next();
-				for (int j=0; j<this.numberOfSequences; j++){
+				for (int j = 0; j < this.numberOfSequences; j++) {
 					// Read value of distance matrix
-					distMatrix[i*numberOfSequences + j] = scanner.nextInt();
+					distMatrix[i * numberOfSequences + j] = scanner.nextInt();
 				}
 			}
-		}			
+		}
 	}
-	
-	float similarity(String name1, String name2) throws SALSAException {
+
+	public final float similarity(String name1, String name2) throws SALSAException {
 		Integer index1 = null, index2 = null;
 
-		for (int i = 0; i < numberOfSequences; i++) {
-			if (this.names[i].equals(name1) || (">" + this.names[i]).equals(name1)){
+		for (int i = 0; i < this.numberOfSequences; i++) {
+			if (this.names[i].equals(name1) || (">" + this.names[i]).equals(name1)) {
 				index1 = i;
 			}
-			if (this.names[i].equals(name2) || (">" + this.names[i]).equals(name2)){
+			if (this.names[i].equals(name2) || (">" + this.names[i]).equals(name2)) {
 				index2 = i;
 			}
 		}
-		
-		if (index1 == null ||
-			index2 == null){
-			throw new SALSAException("Unable to find similarity indexes");
-		}			
 
-		return 1 - distMatrix[index1 * numberOfSequences + index2];
+		if (index1 == null || index2 == null) {
+			throw new SALSAException("Unable to find similarity indexes");
+		}
+
+		return 1 - this.distMatrix[index1 * this.numberOfSequences + index2];
 	}
 
-	SubstitutionMatrix createSubstitutionMatrix(final String matrixSerie, float GEP) throws SALSAException, IOException {
+	/**
+	 * It calculates the average percentage of identity of the sequences and based on that it choose the correct substitution matrix
+	 * 
+	 * @param matrixSerie
+	 * @param GEP
+	 * @return
+	 * @throws SALSAException
+	 * @throws IOException
+	 */
+	public final SubstitutionMatrix createSubstitutionMatrix(final String matrixSerie, float GEP)
+			throws SALSAException, IOException {
 		float pid = 0.0f;
 
-		for (int i = 0; i < numberOfSequences; i++) {
-			for (int j = i + 1; j < numberOfSequences; j++) {
-				pid += distMatrix[i*numberOfSequences + j];
+		for (int i = 0; i < this.numberOfSequences; i++) {
+			for (int j = i + 1; j < this.numberOfSequences; j++) {
+				pid += this.distMatrix[i * this.numberOfSequences + j];
 			}
 		}
 
-		pid /= numberOfSequences * (numberOfSequences - 1) / 2;
+		pid /= this.numberOfSequences * (this.numberOfSequences - 1) / 2;
 		return SubstitutionMatrix.getSubstitutionMatrix(matrixSerie, pid, GEP, null);
-	}	
+	}
 }

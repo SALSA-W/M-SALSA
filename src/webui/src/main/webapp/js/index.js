124915a9b81381f966a674dd22f5f58cc17bc9cb
Fabio Cesarato <fabio.cesarato@gmail.com>
2016-01-30 18:52:56 +0100

    [FIX] Enusure to perform correct validation on index
    Check if has been set at least 3 uniprot ids

--- src/webui/src/main/webapp/js/index.js
+++ src/webui/src/main/webapp/js/index.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
         // If any of these fields are valid
         if ($("textarea[name=inputText]").parsley().isValid() || 
             $("input[name=inputFile]").parsley().isValid() ||
-            $('#dynamicList').children('li').length > 0) 
+            $('#dynamicList').children('li').length > 2) 
         {
             // Remove the error message
             $('.invalid-form-error-message').html('');
@@ -35,20 +35,22 @@ $(document).ready(function() {
             // We already validated each field, so one is filled.
             // Also, destroy parsley's object
             $("textarea[name=inputText]").removeAttr('required').parsley().destroy();
-            $("input[name=inputFile]").removeAttr('required').parsley().destroy();            
+            $("input[name=inputFile]").removeAttr('required').parsley().destroy();
+            $("input[name=newitem]").removeAttr('required').parsley();
             return;
         }
 
         // If none is valid, add the validation to them all
         $("textarea[name=inputText]").attr('required', 'required').parsley();
         $("input[name=inputFile]").attr('required', 'required').parsley();
+        $("input[name=newitem]").attr('required', 'required').parsley();
 
         // stop form submission
         formInstance.submitEvent.preventDefault();
 
         // and display a gentle message
         $('#validation-errors').show();
-        $('#validation-errors-message').html("An input text or a file is required to perfrom alignment");
+        $('#validation-errors-message').html("An input text or at least 3 UniProt Ids or a file is required to perfrom alignment");
         return;
     });
     
